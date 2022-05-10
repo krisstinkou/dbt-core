7cc831edb4c85c31f150b9a58a7c47754862b3cf
Siarhei Nekhviadovich <Siarhei_Nekhviadovich@epam.com>
2022-05-10 14:20:07 +0200

    Bump version v1.1.1epam2

--- core/dbt/version.py
+++ core/dbt/version.py
@@ -234,5 +234,5 @@ def _get_adapter_plugin_names() -> Iterator[str]:
             yield plugin_name
 
 
-__version__ = "1.1.1epam1"
+__version__ = "1.1.1epam2"
 installed = get_installed_version()
--- tests/adapter/dbt/tests/adapter/__version__.py
+++ tests/adapter/dbt/tests/adapter/__version__.py
@@ -1,1 +1,1 @@
-version = "1.1.1epam1"
+version = "1.1.1epam2"
