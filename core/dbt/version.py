0c93d605ff74c24e8fec3c5fe93070aba3efd352
Siarhei Nekhviadovich <sovalinux@gmail.com>
2023-04-21 15:24:25 +0200

    Bump version: 1.2.2epam8 → 1.2.2.8

--- core/dbt/version.py
+++ core/dbt/version.py
@@ -235,5 +235,5 @@ def _get_adapter_plugin_names() -> Iterator[str]:
             yield plugin_name
 
 
-__version__ = "1.2.2epam8"
+__version__ = "1.2.2"
 installed = get_installed_version()
--- tests/adapter/dbt/tests/adapter/__version__.py
+++ tests/adapter/dbt/tests/adapter/__version__.py
@@ -1,1 +1,1 @@
-version = "1.2.2epam8"
+version = "1.2.2"
